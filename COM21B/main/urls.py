23ae05fd4c1b6967536ee286b285c5e591f5c217
Gulaiym <90143818+Ibrmva@users.noreply.github.com>
2022-05-29 19:13:54 +0600

    Delete COM21B directory

--- COM21B/main/urls.py
+++
@@ -1,8 +0,0 @@
-from django.urls import path
-from . import views
-
-from . import views
-urlpatterns = [
-    path('', views.index, name='Home'),
-    path('about', views.about, name='About')
-]
--- COM21B/main/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-def index(request):
-    return render(request, "main/index.html")
-
-def about(request):
-    return render(request, 'main/about.html')
-# Create your views here.
